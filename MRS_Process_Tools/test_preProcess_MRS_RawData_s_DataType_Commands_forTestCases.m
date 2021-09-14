641bb7d29172fd4bd1c584f9e036e65eb37e62bc
rmekle <ralf.mekle@charite.de>
2021-09-14 17:40:23 +0200

    Added file with commands for changing test cases for preProcess_MRS_RawData_s.m

---
+++ MRS_Process_Tools/test_preProcess_MRS_RawData_s_DataType_Commands_forTestCases.m
@@ -0,0 +1,32 @@
+dataType = 'mrs_w';
+filename = filename_svs_slaser_dkd_mrs_w;
+filenamew = filename_w_svs_slaser_dkd_mrs_w;
+dirString = [dirStringBase dataType '/'];
+
+
+% Optional, if water unsuppressed signal exists
+strOVS_w	= 'woutOVS';		% 'wOVS';
+
+
+% Optional, if OVS setting for acquisition of MR spectrum was different
+strOVS		= 'woutOVS';		% 'wOVS';
+
+
+% Optional, if sequence type for acquisition of MR spectrum was different
+seqType		= 'PRESS';			% 'sLASER';
+
+
+% Optional, if any of the following parameters should be changed
+aaDomain		= 't';			% 'f';
+iterin			= 20;			% Any number probably up to 100
+nSD				= 4.0;			% 3.2;		% 2.6;
+plotSwitch		= 1;			% 0;
+reportSwitch	= 0;			% 1;
+strMinUserIn	= 'n';			% 'y';
+tmaxin			= 0.2;			% 
+
+
+
+
+
+
