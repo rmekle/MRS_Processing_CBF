4c5a48616bfbc3f3a0a56f0c9b8cbef1ac60598f
rmekle <ralf.mekle@charite.de>
2022-04-08 18:18:04 +0200

    Adjusted paths to directories for MEGA-PRESS analysis data

--- MRS_MetabQuant/prepare_MetabQuant_s.m
+++ MRS_MetabQuant/prepare_MetabQuant_s.m
@@ -4,7 +4,7 @@
 %
 %% Script to prepare metabolite quantification of magnetic resonance spectroscopy (MRS) data
 %
-% Ralf Mekle, Charite Universitätsmedizin Berlin, Germany, 2020, 2021; 
+% Ralf Mekle, Charite Universitätsmedizin Berlin, Germany, 2020, 2021, 2022; 
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -25,7 +25,7 @@ sMsg_newLines		= sprintf('\n\n');
 filename_In				= '';
 filename_w_In			= '';
 strVOI					= 'HC';			% 'HC';		% 'PCG';
-seqType_MRS				= 'sLASER';		% 'SPECIAL';	% 'MEGA-PRESS'; % 'sLASER';
+seqType_MRS				= 'MEGA-PRESS';		% 'SPECIAL';	% 'MEGA-PRESS'; % 'sLASER';
 dataType_MRS			= 'mrs_w_ref';
 % strOVS_In				= 'wOVS';
 % strOVS_w_In				= 'wOVS';
@@ -51,12 +51,13 @@ switch seqType_MRS
 		% pre-processing of MR spectra
 		switch(noSD_In)
 			case(2.6)
-				dirString_In			= '/home/mekler/CSB_NeuroRad/mekler/Ralf/CSB_Projects/MRS_Dopamin/MRS_DOPA_Z_Analysis/DOPA_FID-A_SD_2_6/';
+				%dirString_In			= '/home/mekler/CSB_NeuroRad/mekler/Ralf/CSB_Projects/MRS_Dopamin/MRS_DOPA_Z_Analysis/DOPA_FID-A_SD_2_6/';
+				dirString_In			= '/home/mekler/CSB_NeuroRad/mekler/Data_II_Analysis/3T_BCAN_MRS_Dopamin_Analysis/Z_DOPA_FID-A_SD_2_6/';
 			case(3.2)
-				%dirString_In			= '/home/mekler/CSB_NeuroRad/mekler/Ralf/CSB_Projects/MRS_Dopamin/MRS_DOPA_Z_Analysis/DOPA_FID-A_SD_3_2/';
-				dirString_In			= '/home/mekler/CSB_NeuroRad/mekler/Ralf/CSB_Projects/MRS_Dopamin/MRS_DOPA_Z_Analysis/DOPA_FID-A_SD_3_2_New/';
+				dirString_In			= '/home/mekler/CSB_NeuroRad/mekler/Data_II_Analysis/3T_BCAN_MRS_Dopamin_Analysis/Z_DOPA_FID-A_SD_3_2/';
+				%dirString_In			= '/home/mekler/CSB_NeuroRad/mekler/Data_II_Analysis/3T_BCAN_MRS_Dopamin_Analysis/Z_DOPA_FID-A_SD_3_2_New/';
 			case(4.0)
-				dirString_In			= '/home/mekler/CSB_NeuroRad/mekler/Ralf/CSB_Projects/MRS_Dopamin/MRS_DOPA_Z_Analysis/DOPA_FID-A_SD_4_0/';
+				dirString_In			= '/home/mekler/CSB_NeuroRad/mekler/Data_II_Analysis/3T_BCAN_MRS_Dopamin_Analysis/Z_DOPA_FID-A_SD_4_0/';
 			
 			otherwise
 				error('%s: ERROR: No directory/data for noSD_In =  %f!', sFunctionName, noSD_In);
