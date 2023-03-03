8232057ccc88aaf7e340485b2d13187e2ac6362a
rmekle <ralf.mekle@charite.de>
2023-03-03 12:30:11 +0100

    Updated copying of table with quantification resutls into Excel, so that columns can be deleted

--- MRS_MetabQuant/copyMetabResults_IntoExcel_s.m
+++ MRS_MetabQuant/copyMetabResults_IntoExcel_s.m
@@ -9,7 +9,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % USAGE
-% [resultsTable_final] = copyMetabResults_IntoExcel_s(fullFileName_In,noLastRowsToDel,newColsTable,noColsToMove,strOutDir,strSheet,strRange,bWriteVariableNames,bUseTemplate,fullPath_Template);
+% [resultsTable_final] = copyMetabResults_IntoExcel_s(fullFileName_In,noLastRowsToDel,bDeleteCols,indColsToDel,bMoveCols,noColsToMove,newColsTable,strOutDir,strSheet,strRange,bWriteVariableNames,bUseTemplate,fullPath_Template);
 % 
 % DESCRIPTION:
 % Function that reads in results from metabolite quantification from a (.csv) file into a
@@ -21,8 +21,13 @@
 % fullFileName_In	= String variable for the full filename (path) to the (.csv) file
 %						that contains the original results from metabolite quantification
 % noLastRowsToDel	= Integer, # of last rows to be deleted from original table of results
-% newColsTable		= Integer, # of columns to be added to original table of results
+% bDeleteCols		= Boolean that specifies whether to delete columns from original table
+%						of results
+% indColsToDel		= Indices of columns to be deleted from original table of results
+% bMoveCols			= Boolean that specifies whether to move columns during reordering of 
+%						new table
 % noColsToMove		= integer, # of colums to be moved during reordering of new table
+% newColsTable		= Integer, # of columns to be added to original table of results
 % strOutDir			= String variable for the name of the output directory, i.e. the 
 %						directory, where all output files are saved to
 % strSheet			= String variable that specifies name of sheet in Excel file, 
@@ -46,7 +51,7 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [resultsTable_final] = copyMetabResults_IntoExcel_s(fullFileName_In,noLastRowsToDel,newColsTable,noColsToMove,strOutDir,strSheet,strRange,bWriteVariableNames,bUseTemplate,fullPath_Template)
+function [resultsTable_final] = copyMetabResults_IntoExcel_s(fullFileName_In,noLastRowsToDel,bDeleteCols,indColsToDel,bMoveCols,noColsToMove,newColsTable,strOutDir,strSheet,strRange,bWriteVariableNames,bUseTemplate,fullPath_Template)
 
 %% Clear all variables from workspace and close all figures
 % clear all;
@@ -61,7 +66,7 @@ disp(sMsg_newLines);
 
 
 %% Check on # of input arguments and assign default values to variables, if required
-maxNargin	= 10;
+maxNargin	= 13;
 if nargin<maxNargin
 	fullPath_Template = '';
 end
@@ -80,23 +85,40 @@ opts_read						= detectImportOptions(fullFileName_In);
 opts_read.EmptyLineRule			= 'read';
 opts_read.VariableNamingRule	= 'preserve';
 resultsTable					= readtable(fullFileName_In, opts_read);
+%resultsTable_orig				= resultsTable;
 
 % Delete selected # of last rows in table of results, e.g rows that contain a blank line, 
 % mean and STD computed by Matlab, since this information should be recomputed in Excel
 sz_Results					= size(resultsTable);
 % resultsTable_withSD		= resultsTable;
-resultsTable([(sz_Results(1)-(noLastRowsToDel-1)):sz_Results(1)], :)		= [];
+resultsTable((sz_Results(1)-(noLastRowsToDel-1)):sz_Results(1), :)		= [];
+sz_Results					= size(resultsTable);
 
+
+% Delete specified columns from table of results, if selected
+if bDeleteCols
+	resultsTable(:, indColsToDel)	= [];
+	sz_Results						= size(resultsTable);
+end
+
+% Init final table of results
+%resultsTable_final	= resultsTable;
 
 % Insert selected # of columns for additional parameters, e.g. water linewidth (LW_H2O),
 % into table of results by concatenating new input table with table of results
 % and use # of colums to be moved from original table of results for reordering of columns
-%  within resulting table to obtain desired order of columns
-sz_newCols			= size(newColsTable);
-noNewCols			= sz_newCols(2);
-resultsTable_final	= [newColsTable resultsTable];
+%  within resulting table to obtain desired order of columns, if selected
+if bMoveCols
+	sz_newCols			= size(newColsTable);
+	noNewCols			= sz_newCols(2);
+	resultsTable_final	= [newColsTable resultsTable];
+	sz_final			= size(resultsTable_final);
+	resultsTable_final	= resultsTable_final(:, [[noNewCols+(1:noColsToMove)] [1:noNewCols] [(noColsToMove+noNewCols+1):sz_final(2)]]);
+else
+	resultsTable_final	= resultsTable;
+end
 sz_final			= size(resultsTable_final);
-resultsTable_final	= resultsTable_final(:, [[noNewCols+(1:noColsToMove)] [1:noNewCols] [(noColsToMove+noNewCols+1):sz_final(2)]]);
+
 
 % Write table of results to (formatted) Excel file
 % Determine filename for Excel file
