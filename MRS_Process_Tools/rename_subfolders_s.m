a5ba980a1f4267a444fef057c4eecb6084df8537
rmekle <ralf.mekle@charite.de>
2023-04-19 19:52:01 +0200

    Completed routine to rename subdirectories of MRS DICOM data

--- MRS_Process_Tools/rename_subfolders_s.m
+++ MRS_Process_Tools/rename_subfolders_s.m
@@ -22,7 +22,8 @@ disp(sMsg_newLines);
 
 %% Init input parameters for renaming subfolders
 parentDir_Base		= '/home/mekler/CSB_NeuroRad/mekler/Data_II/3T_BCAN_MRS_Trauma/';
-parentDir_AddOn		= 'MRS_Trauma_00_All_DICOM_Test/';	% 'MRS_Trauma_00_All_DICOM/';
+parentDir_AddOn		= 'MRS_Trauma_00_All_DICOM_New/';	% 'MRS_Trauma_00_All_DICOM/'; 
+					% 'MRS_Trauma_00_All_DICOM_Test/'; % 'MRS_Trauma_00_All_DICOM_New/';
 parentDir			= [parentDir_Base, parentDir_AddOn];
 
 % Select start and end paatern for substring extraction
@@ -38,19 +39,39 @@ end
 
 % List subfolders in parent directory and only include folders (directories) and exclude
 % Linux directories '.' and '..'
-list_subfolders			= dir(parentDir);
+list_subfolders			= dir_s(parentDir);
 list_subfolders			= list_subfolders([list_subfolders.isdir]);
 list_subfolders_name	= {list_subfolders.name};
-list_subfolders_name(strncmp(list_subfolders_name, '.', 1)) = [];
+%list_subfolders_name(strncmp(list_subfolders_name, '.', 1)) = [];
 
 % Loop over all subfolders
 % For each subfolder, extract desired substring (e.g. pseudonym for subject) from name of
 % subfolder assuming that such information is included
 % Then, add extracted substring to all folder names within each subfolder of parent
 % directory
+newChr		= cell(1);
+newName		= '';
 for iFolder = 1:1:numel(list_subfolders_name)
 	% Extract desired substring from name of subfolder
 	newChr		= extractBetween(list_subfolders_name{iFolder}, startPat, endPat);
+	if isempty(newChr)
+		error('%s: Error! Extracted substring newChr = %s is empty!\n', sFunctionName, newChr{1,1})
+	end
 
+	% Determine list of directories and files in current subdirectory
+	subDir						= fullfile(parentDir, list_subfolders_name{iFolder});
+	list_subfolders_sub			= dir_s(subDir);
+	list_subfolders_sub_name	= {list_subfolders_sub.name};
 
-end
+	% Add extracted substring to names of directories and files in current subfolder and
+	% rename all of them
+	for iSub = 1:1:numel(list_subfolders_sub_name)
+		newName		= [newChr{1, 1}, list_subfolders_sub_name{iSub}];
+		%disp([fullfile(subDir, list_subfolders_sub_name{iSub}) fprintf('') fullfile(subDir, newName)]);
+		[status,msg]	= movefile( fullfile(subDir, list_subfolders_sub_name{iSub}), fullfile(subDir, newName) );
+		if status ~= 1
+			error('%s: Error moving/renaming file %s!\n\n%s', sFunctionName, list_subfolders_sub_name{iSub}, msg);
+		end
+
+	end			% End of for iSub = 1:1:numel(list_subfolders_sub_name)
+end		% End of for iFolder = 1:1:numel(list_subfolders_name)
