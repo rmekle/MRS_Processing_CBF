1e88108e8e2e9bce643d4ecdb18e264675c9c26a
rmekle <ralf.mekle@charite.de>
2024-06-12 18:15:42 +0200

    Corrected input paramter in call of routine measure_MRS_SNR_LW_FIDA_s.m

--- MRS_GetSNR_LW/measure_MRS_ListOfFiles_SNR_LW_s.m
+++ MRS_GetSNR_LW/measure_MRS_ListOfFiles_SNR_LW_s.m
@@ -25,7 +25,7 @@ fprintf('\n\n');
 %% Init input parameters for easuring SNR and Linewidth (LW) in list of files of MRS data
 dirString_In			= '/home/mekler/CSB_NeuroRad/mekler/Data_II/Z_Test_Data/Test_MRS_SNR_LW/';
 %dirString_In			= '/home/mekler/CSB_NeuroRad/mekler/Data_II_Analysis/3T_BCAN_MRS_Trauma_Analysis/PCG_dat_FID-A_SD_3_2_ECCref_ls3_SR1/PCG_LCModel_Data_MRS_only/';
-filename_MRS_In			= '';
+filename_MRS_In			= '3T_SBA_C_0008_20210325_meas_MID00333_FID95325_svs_slaser_dkd_HC_TE23_WS256_wOVS_3.2_processed_lcm.RAW';
 filename_w_In			= '';
 dataFormat_MRS_In		= 'lcmRAW';
 signal_ppmRange_In		= [1.8, 2.2];
@@ -45,45 +45,39 @@ TE_In					= [];
 TR_In					= [];
 
 
-
-%[data_MRS, SNR, FWHM, info]	= measure_MRS_SNR_LW_FIDA_s(dirString, filename_MRS, filename_w, dataFormat_MRS, signal_ppmRange, noise_ppmRange, LWpeak_ppmRange, zp_factor, outDirString, dataType_MRS, bOutFile, plotswitch, seqType_MRS, procParams, Bo_field, spectralWidth, TE, TR);
-
-
-
-
-
-
-
-
-%% Obtain information about the list of files (.dat) or list of directories (.IMA)
-% (assuming that all .dat files are included in the same directory)
+%% Obtain information about the list of files for (preprocessed) MR spectra
+% (assuming that all files are included in the same directory)
 % (On Linux, file list in Matlab also includes the two directories "." and "..", which
 % means that the actual # of files in the directory is (# of entries in list - 2;
 % however, if dir is used to list specific files, e.g. using a file extension, these two 
 % directories are not included in the resulting list)
 %cd(dirString_In);
 
-% % FLAG: CHANGE
-% % Added an option for loading a set of directories containing scans
-% switch fileExtension
-%     case 'dat'
-%         structFileListing		= dir([dirString_In, '*.dat']);
-%         noEntriesListing		= length( structFileListing );
-%         %noDataFiles				= noEntriesListing - 2
-%     case 'IMA'
-%         structFileListingAll	= dir(dirString_In);
-%         subDir					= [structFileListingAll(:).isdir];
-%         structFileListing		= structFileListingAll(subDir);
-%         % Remove the two directories '.' and '..'
-%         structFileListing		= structFileListing(~ismember({structFileListing(:).name},{'.','..'}));
-%         noEntriesListing		= length(structFileListing);
-%     otherwise
-%         error('%s: ERROR: Unknown file extension %s!', sFunctionName, fileExtension);
-% end
+
+% Added an option for loading a set of directories containing scans
+switch dataFormat_MRS_In
+    case 'dat'
+        structFileListing		= dir([dirString_In, '*.dat']);
+        noEntriesListing		= length( structFileListing );
+        %noDataFiles				= noEntriesListing - 2
+    case 'IMA'
+        structFileListingAll	= dir(dirString_In);
+        subDir					= [structFileListingAll(:).isdir];
+        structFileListing		= structFileListingAll(subDir);
+        % Remove the two directories '.' and '..'
+        structFileListing		= structFileListing(~ismember({structFileListing(:).name},{'.','..'}));
+        noEntriesListing		= length(structFileListing);
+    otherwise
+        error('%s: ERROR: Unknown dataFormat_MRS_In %s!', sFunctionName, dataFormat_MRS_In);
+end
 
 
 
 %% Preprocess all data files depending on sequence type
+
+
+[data_MRS, SNR, FWHM, info]	= measure_MRS_SNR_LW_FIDA_s(dirString_In, filename_MRS_In, filename_w_In, dataFormat_MRS_In, signal_ppmRange_In, noise_ppmRange_In, LWpeak_ppmRange_In, zp_factor_In, outDirString_In, dataType_MRS_In, bOutFile_In, plotswitch_In, seqType_MRS_In, procParams_In, Bo_field_In, spectralWidth_In, TE_In, TR_In);
+
 
 
 
