58be383de560807ef5a8ad653261167627cc7455
rmekle <ralf.mekle@charite.de>
2024-10-30 17:37:58 +0100

    Created function body for function to complete directory name of processed MRS data depending on input parameter settings

---
+++ MRS_Process_Tools/completeDirName_MRS_processed_s.m
@@ -0,0 +1,6 @@
+function [outputArg1,outputArg2] = completeDirName_MRS_processed_s(inputArg1,inputArg2)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end		% End of function
