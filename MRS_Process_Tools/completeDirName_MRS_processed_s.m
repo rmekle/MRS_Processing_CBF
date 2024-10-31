1d1450a3c834016f3b4d070626dfde28363742e7
rmekle <ralf.mekle@charite.de>
2024-10-31 18:19:02 +0100

    Started to add comments for input and output arguments of function

--- MRS_Process_Tools/completeDirName_MRS_processed_s.m
+++ MRS_Process_Tools/completeDirName_MRS_processed_s.m
@@ -1,5 +1,121 @@
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% completeDirName_MRS_processed_s.m
+%
+%% Function to preprocess single volume magnetic resonance spectroscopy (MRS) data
+%  using functions from the MRS processing toolkit FID-A
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% USAGE
+% [strDir_Out] = completeDirName_MRS_processed_s(strDir_Out_Base, fileExt, strVOI, dataType, signals, leftshift, avgBlockSize, rmbadav, noSD, strSpecReg, driftCorr, bECC, strProcessTool)
+% 
+% DESCRIPTION:
+% Function for processing Siemens MRS data in .dat format (twix raw data) or in .IMA
+% format (DICOM data)
+% using functions from the MRS processing toolkit FID-A
+% Includes combination of receiver coil channels (if required), removal of bad averages, 
+% frequency drift correction, averaging, eddy current correction (optional), phase and 
+% frequency correction.
+% 
+% INPUTS:
+% dirString    = String variable for the name of the directory containing
+%                   the water suppressed .dat file or .IMA files
+% dirString_w  = (Optional) ['DirectoryWater'] String variable for the name of the directory containing
+%                   the water unsuppressed .dat file or .IMA files,
+%                   optional because water unsupressed data is optional and
+%                   dat file can also be in dirString
+% outDirString = String variable for the name of the output directory, i.e. the directory,
+%					where all output files are saved to
+% filename     = (Optional) ['Filename'] String variable for the name of the water suppressed .dat file,
+%					e.g. 3T_20170510_PetraO_meas_MID00091_FID153257_rm_special_RF_ACC.dat
+%					or specialDLPFC.dat - Optional because not necessary
+%					for .IMA data
+% filename_w   = (Optional) ['UnsupressedFilename'] String variable for the name of the water unsuppressed .dat file,
+%					e.g. 3T_20170510_PetraO_meas_MID00091_FID153259_rm_special_RF_ACC.dat
+%					or specialDLPFC_w.dat - Optional because water
+%					unsupressed signal is optional and water unsupressed
+%					can be a directory containing .IMA files
+% filename_r   = (Optional) ['ReportFilename'] String variable for the name
+%                   of the file - Optional because the name will just be
+%                   automatically generated otherwise
+% seqType	   = String specifying the MRS sequence type used for data acquisition, e.g.
+%					'PRESS', 'STEAM', 'SPECIAL', 'sLASER', 'MEGA-PRESS'
+% dataType	   = String describing the type of MRS data:
+%					'mrs'		= MR spectrum without water signal, 
+%					'mrs_w'		= MR spectrum with unsuppressed water signal
+%					'mrs_w_ref'	= MR spectrum with unsuppressed water signal and reference
+%									(water) scans
+%					'mrs_ref'	= MR spectrum with reference (water) scans
+%					'water'		= MR spectrum is unsuppressed water signal itself
+%					'water_ref' = MR spectrum is unsuppressed water signal itself with
+%									reference (water) scans (should be very rare!)
+% strOVS	   = (optional) ['OVS'] Character array that specifies whether MR spectrum acquired 
+%					 with OVS ('wOVS') or withoutOVS ('woutOVS') is used for processing. 
+%					 Default is 'woutOVS', which means that OVS was not used.
+% strOVS_w	   = (optional) ['OVSwater'] Character array that specifies whether water acquired with OVS ('wOVS')
+%					 or withoutOVS ('woutOVS') is used for processing.
+%					 Default is 'woutOVS', which means that OVS was not used.
+% leftshift	   = (optional) ['Leftshift'] # of points to leftshift all FIDs, i.e. to remove leading 
+%					 datapoints from the FID  to get rid of 1st order phase. Default is 0.
+% leftshift_w  = (optional) ['WaterLeftshift'] # of points to leftshift all water unsupressed FIDs, i.e. to remove leading 
+%					 datapoints from the FID  to get rid of 1st order phase. Default is the same as leftshift.
+% avgBlockSize = (Optional) ['avgBlockSize'] Block size of averages used for block
+%					averaging prior to processing for noisy data. If equal to 0, block
+%					averaging is not applied. Default value is 0.
+% rmbadav	   = (Optional) ['RemoveBadAverages'] Character array that specifies whether 
+%					removal of bad averages should be performed or not. 
+%					Default is 'y'.
+% nSD		   = (Optional) ['StandardDeviation'] # of standard deviations for bad average removal. Default
+%					value is 3.2.
+% strSpecReg   = (Optional) ['SpectralRegistrationID'] Character array that specifies ID,
+%					i.e. name, of spectral registration (drift correction) that might be 
+%					performed to distinguish results. Default is 'SR00'.
+% driftCorr	   = (Optional) ['DriftCorrection'] Character array that specifies whether 
+%					spectral registration (drift correction) should be performed or not. 
+%					Default is 'y'.
+% iterin       = (Optional) ['Iterations']  Maximum number of allowed iterations for the spectral
+%                   registration to converge. Default is 20.
+% aaDomain     = (Optional) ['aaDomain'] Perform the spectral registration (drift correction) using
+%                   the full spectrum ('t'), or only a limited frequency range ('f').  Default is 'f'.
+% tmaxin       = (Optional) ['DriftCorrectionDuration'] Duration (in sec.) of the time domain signal
+%                   used in the spectral registration (drift correction).
+%                   Default is 0.2 sec.
+% bTmaxSet	   = (Optional) ['MaxTimeAlignmentSet'] Boolean, if 1, the given value for
+%					tmaxin is used; if 0, the routine that aligns all averages determines
+%					tmax from the data (currently only implemented for the time domain).
+%					Default is 1.
+% medin		   = (Optional) ['medianALignment] Selects reference, to which all averages
+%					are aligned to; 'y', 'n', 'a' or 'ref' are possible; see
+%					op_alignAverages_fd.m and op_alignAverages.m from FID-A for details.
+%					Default is 'y'.
+% ppmmin_fix   = (Optional) ['ppmMinimum_fix'] Initial minimum ppm value, for which
+%					spectral registration is applied in the frequency domain. 
+%					Default is 1.6.
+% ppmmaxarreay_fix = (Optional) ['ppmMaximumArray_fix'] Initial array of maximum ppm
+%					values, for which spectral registration is applied in the frequency
+%					domain. Default is [3.5; 4.0; 5.5].
+% bECC 		   = (optional) ['ECC'] Boolean that specifies whether eddy current correction (ECC) 
+%					 should be performed or not. Default is 0.
+% bPhaseCorrFreqShift = (Optional) ['PhaseFrequencyCorrection'] Boolean that specifies whether phase correction and
+%					frequency shifting should be performed or not. Default is 0.
+% strMinUserIn = (Optional) ['MinimizeUserInput'] String that specifies whether user input/interaction should be
+%					 minimized or not; 'y' or 'Y' lead to minimization, 'n' or 'N' do not
+%					Default is 'y'.
+% plotSwitch   = (Optional)	['ShowPlots'] Switch for displaying plots: 1 = ON, 0 = OFF. Default is 0
+% reportSwitch = (Optional) ['GenerateReport'] Switch for generating an html report with corresponding  
+%					figures and a readme file: 1 = ON, 0 = OFF. Default is 1. 
+% 
+% OUTPUTS:
+% strDir_Out		= Fully preprocessed, water suppressed output spectrum
+%
+%
+% Ralf Mekle, Charite Universitätsmedizin Berlin, Germany, 2024;
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [strDir_Out] = completeDirName_MRS_processed_s(strDir_Out_Base, fileExt, strVOI, dataType, signals, leftshift, avgBlockSize, rmbadav, noSD, strSpecReg, driftCorr, bECC, strProcessTool)
-%UNTITLED2 Summary of this function goes here
 
 % Select directory for output data depending on voxel location, data type,
 % # of SDs, and other options used for pre-processing of MR spectra or acquired
