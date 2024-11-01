3536831f875b0900028c287ea529067c1690a1d7
rmekle <ralf.mekle@charite.de>
2024-11-01 12:31:35 +0100

    Replaced call of datestr by call of datatime, since use of datestr no longer recommended by MATLAB

--- MRS_MetabQuant/run_LCModel_ListOfFiles_s.m
+++ MRS_MetabQuant/run_LCModel_ListOfFiles_s.m
@@ -1441,7 +1441,9 @@ end		% End of if( noFiles_table > 0 )
 
 %% Save variables of workspace to file
 % Obtain current date and time in specific format
-dt		= datestr(now,'yyyymmdd_HH_MM_SS');
+% Since use of datestr was no longer recommended, code was changed to use datetime instead
+%dt		= datestr(now,'yyyymmdd_HH_MM_SS');
+dt		= char(datetime('now', 'Format', 'yyyyMMdd_HH_mm_ss'));
 
 % Save workspace into output directory (optional with user input)
 % (Extension".mat" in filename explicitly required, so that Matlab can correctly load 
