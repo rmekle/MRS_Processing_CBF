fc00fe796ef34df9b6d78cbc1397358272eac93a
rmekle <ralf.mekle@charite.de>
2025-03-04 12:32:43 +0100

    Added data and analysis directory selection for 3T_TGA MRS study

--- MRS_Process_Tools/preprocess_ListOfFiles_s.m
+++ MRS_Process_Tools/preprocess_ListOfFiles_s.m
@@ -227,6 +227,25 @@ switch seqType_MRS
 					otherwise
 						error('%s: ERROR: Unknown file extension (data type) %s!', sFunctionName, fileExt_MRS);
 				end			% End of switch fileExt_MRS
+			case '3T_TGA'
+				% Data (input) directories
+				dirString_In_Base		= '/home/mekler/CSB_NeuroRad/mekler/Data_II/3T_MRS_TGA/';
+
+				% Output data directory
+				dirString_Out_Base		= '/home/mekler/CSB_NeuroRad/mekler/Data_II_Analysis/3T_MRS_TGA_Analysis/';
+
+				% Directories depending on MRS data type
+				switch fileExt_MRS
+					case 'dat'
+						% Select directories specific to MRS raw data (.dat)
+						dirString_In_AddOn1		= sprintf('MRS_TGA_00_All_RawData_dat_Files_MRS_%s', strVOI_MRS);
+					case 'IMA'
+						% Select directories specific to MRS DICOM data (.IMA)
+						dirString_In_AddOn1		= sprintf('MRS_TGA_00_All_DICOM_IMA_Files_MRS_%s', strVOI_MRS);
+
+					otherwise
+						error('%s: ERROR: Unknown file extension (data type) %s!', sFunctionName, fileExt_MRS);
+				end			% End of switch fileExt_MRS
 			case '7T_KCL'
 				% Data (input) directories
 				dirString_In_Base		= '/home/mekler/CSB_NeuroRad/mekler/Data_II/7T_KCL/';
