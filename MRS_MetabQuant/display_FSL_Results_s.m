d4abeaa6c40c168dc53a535a7490adeb27356378
rmekle <ralf.mekle@charite.de>
2025-03-07 12:13:32 +0100

    Added option to display results from segmentation for different choices of center of brain coordinates for 3T_TGA MRS study

--- MRS_MetabQuant/display_FSL_Results_s.m
+++ MRS_MetabQuant/display_FSL_Results_s.m
@@ -65,6 +65,7 @@ switch seqType
 				inputDirImages 		= [inputDirBase,'MRS_TGA_00_All_MPRAGE_NIfTI/'];
 				%inputDirOverlays	= [inputDirBase,'MRS_TGA_00_All_MPRAGE_NIfTI_Segmented/3T_TGA_bet_BrainCenter_87_115_180_fractThrash_0_3/'];
 				inputDirOverlays	= [inputDirBase,'MRS_TGA_00_All_MPRAGE_NIfTI_Segmented/3T_TGA_bet_BrainCenter_96_115_170_fractThrash_0_3/'];
+				%inputDirOverlays	= [inputDirBase,'MRS_TGA_00_All_MPRAGE_NIfTI_Segmented/3T_TGA_bet_BrainCenter_90_115_150_fractThrash_0_3/'];
 
 			otherwise
 				error('%s: ERROR: Unknown study %s!', sFunctionName, strStudy);
