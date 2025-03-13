7fcbd6caaf6110f7435906e09558c43414eb561f
rmekle <ralf.mekle@charite.de>
2025-03-13 19:08:10 +0100

    Modified name of parent directory to rename DICOM files of linewidth (LW) measurements with sLASER

--- MRS_Process_Tools/rename_files_MRS_DICOM_s.m
+++ MRS_Process_Tools/rename_files_MRS_DICOM_s.m
@@ -27,7 +27,8 @@ sFunctionName		= 'rename_files_MRS_DICOM_s';
 %parentDir_AddOn		= 'MRS_Trauma_00_All_DICOM_IMA_Files_MRS_HC/';	
 					% 'MRS_Trauma_00_All_DICOM_IMA_Files_MRS_PCG/';	% 'MRS_Trauma_00_All_DICOM_IMA_Files_MRS_HC/';
 parentDir_Base		= '/home/mekler/CSB_NeuroRad/mekler/Data_II/3T_MRS_TGA/';
-parentDir_AddOn		= 'MRS_TGA_00_All_DICOM_IMA_Files_SUM_MRS_HC/';
+%parentDir_AddOn		= 'MRS_TGA_00_All_DICOM_IMA_Files_SUM_MRS_HC/';
+parentDir_AddOn		= 'MRS_TGA_00_All_DICOM_IMA_Files_LW_HC/';
 %parentDir_AddOn		= 'TGA_Test/';	
 parentDir			= [parentDir_Base, parentDir_AddOn];
 
