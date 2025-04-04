569eb343636b5e65f9d0b22d514c305627b63247
rmekle <ralf.mekle@charite.de>
2025-04-04 11:54:14 +0200

    Changed settings for LCModel control file for 3T_TGA MRS study

--- MRS_MetabQuant/run_LCModel_ListOfFiles_s.m
+++ MRS_MetabQuant/run_LCModel_ListOfFiles_s.m
@@ -532,19 +532,19 @@ switch seqType_MRS
 								LCM_Control						= '3T_RAW4093_sLASER_TE23_HC_water_noECC_TGA_42677_mac_nratio0_v3';
 								LCM_ControlAdd					= '_Con8';
 							case 'IMA'
-								LCM_Control						= '3T_IMA2047_sLASER_TE23_HC_water_noECC_TGA_44889_mac_nratio0_v3';
-								LCM_ControlAdd					= '_Con8_44889';
-								strSheetSel_AddOn				= '_Left_1';
+								% IMA2047 for leftshit = 1
+								%LCM_Control						= '3T_IMA2047_sLASER_TE23_HC_water_noECC_TGA_44889_mac_nratio0_v3';
+								%LCM_ControlAdd					= '_Con8_44889';
+								%strSheetSel_AddOn				= '_Left_1';
 								%LCM_Control						= '3T_IMA2047_sLASER_TE23_HC_water_noECC_TGA_44080_mac_nratio0_v3';
 								%LCM_ControlAdd					= '_Con8_44080';
 								%strSheetSel_AddOn				= '_Right_1';
-								% IMA2047 for leftshit = 1
 								%LCM_Control						= '3T_IMA2047_sLASER_TE23_HC_water_noECC_TGA_43315_mac_nratio0_v3';
 								%LCM_ControlAdd					= '_Con8_43315';
 								%strSheetSel_AddOn				= '_Left_2';
-								%LCM_Control						= '3T_IMA2047_sLASER_TE23_HC_water_noECC_TGA_43278_mac_nratio0_v3';
-								%LCM_ControlAdd					= '_Con8_43278';
-								%strSheetSel_AddOn				= '_Right_2';
+								LCM_Control						= '3T_IMA2047_sLASER_TE23_HC_water_noECC_TGA_43278_mac_nratio0_v3';
+								LCM_ControlAdd					= '_Con8_43278';
+								strSheetSel_AddOn				= '_Right_2';
 
 							otherwise
 								error('%s: ERROR: Unknown file extension (data type) %s!\n', sFunctionName, fileExt_MRS);
