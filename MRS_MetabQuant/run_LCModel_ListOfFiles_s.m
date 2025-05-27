fb4c6a01381e9e14cba7acf10d5496ecbdf1a70d
rmekle <ralf.mekle@charite.de>
2025-05-27 15:05:04 +0200

    Updated commentzs and parameter selection in rouitne run_LCModel_ListOfFiles_s(...)

--- MRS_MetabQuant/run_LCModel_ListOfFiles_s.m
+++ MRS_MetabQuant/run_LCModel_ListOfFiles_s.m
@@ -21,7 +21,7 @@ sFunctionName		= 'run_LCModel_ListOfFiles_s';
 fprintf('\n\n');
 
 
-%% Init input parameters for preprocessing
+%% Init input parameters for selected MRS study including those used for preprocessing
 % Obtain parameter settings for preprocessing from initialization routine
 configSel				= 'config_Study_sLASER_VOI_IMA_MRS_lsN_SDx_y_SR1_ECC';
 %configSel				= 'config_Study_sLASER_VOI_dat_MRS_lsN_SDx_y_SR1_ECC';
