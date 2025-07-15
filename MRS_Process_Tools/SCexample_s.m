424030312550571678d7a0e6955f312998642ed2
rmekle <ralf.mekle@charite.de>
2025-07-15 12:24:07 +0200

    Modified call to perform spectral cross-correlation to spectXcorr_s(...)

--- MRS_Process_Tools/SCexample_s.m
+++ MRS_Process_Tools/SCexample_s.m
@@ -9,7 +9,9 @@ load sampleData;
 %  - spectrum range of 1.8 to 3.6ppm, 
 %  - reference spectrum is the 1st transient
 %  - no apodization to determine freq/phase offsets
-[fidCor,outVal] = spectXcorr(fidm,[1.8 3.6],'f',0,1);
+% RM: Call own version of spectral cross-correlation routine
+%[fidCor,outVal] = spectXcorr(fidm,[1.8 3.6],'f',0,1);
+[fidCor,outVal] = spectXcorr_s(fidm,[1.8 3.6],'f',0,1,sw,sfrq1H,H1offset);
 
 
 
