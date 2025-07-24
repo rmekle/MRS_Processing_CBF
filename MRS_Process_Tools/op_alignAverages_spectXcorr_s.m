69611ce22672e56b498622b5e8c65f5c09d2be2f
rmekle <ralf.mekle@charite.de>
2025-07-24 20:14:12 +0200

    Modified comments

--- MRS_Process_Tools/op_alignAverages_spectXcorr_s.m
+++ MRS_Process_Tools/op_alignAverages_spectXcorr_s.m
@@ -13,7 +13,8 @@
 % 
 % INPUTS:
 % in        = Input data structure
-% dataFlag	= Flag to indicate whether original data was read as conjugate complex or not
+% dataFlag	= Flag to indicate whether original data was read as conjugate complex
+%				('conj') or not ('forw')
 % minppmSC	= Minimum of frequency range (ppm) used for spectral cross-correlation
 % maxppmSC	= Maximum of frequency range (ppm) used for spectral cross-correlation
 % refSC     = Character array to choose reference signal for spectral cross-correlation
