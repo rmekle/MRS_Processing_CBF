8c92c2d02e2d15b366b2da38325777ca77a3f391
rmekle <ralf.mekle@charite.de>
2025-08-01 16:57:24 +0200

    Selected different configuration for preprocessing of MRS data, SC2 instead of SC1

--- MRS_Process_Tools/preprocess_ListOfFiles_s.m
+++ MRS_Process_Tools/preprocess_ListOfFiles_s.m
@@ -22,7 +22,7 @@ fprintf('\n\n');
 
 %% Init input parameters for preprocessing
 % Obtain parameter settings for preprocessing from initialization routine
-configSel				= 'config_Study_sLASER_VOI_IMA_MRS_lsN_SDx_y_SC1_ECC'
+configSel				= 'config_Study_sLASER_VOI_IMA_MRS_lsN_SDx_y_SC2_ECC';
 [sParamsMRS_struct]		= initParams_MRS_s(configSel);
 
 % Set addition to starting index into list of files to be preprocessed; 
